5be7c72c43c14372696238f72ecae44c430a07d0
chaparron <chaparron@github.com>
2020-01-29 17:32:53 +0100

    1:55:30 CRUD acabado

--- src/database.js
+++ src/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://fazt:fazt@cluster0-xyas1.mongodb.net/test?retryWrites=true&w=majority',{
-    useCreateindex: true,
     useNewUrlParser: true,
     useFindAndModify: false, 
     useUnifiedTopology: true
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -31,9 +31,24 @@ router.post('/notes/new-note', async(req,res) =>{
 
 
 router.get('/notes', async(req,res) =>{
-    const notes = await Note.find().lean();//!!el lean hace que se rendericen!!
-    console.log(notes);
+    const notes = await Note.find().lean().sort({date:'desc'});//!!el lean hace que se rendericen!!
     res.render('notes/all-notes', {notes} );
 });
+
+router.get('/notes/edit/:id',async (req,res)=>{
+    const note = await Note.findById(req.params.id).lean()
+    res.render('notes/edit-note',{note})
+})
+
+router.put('/notes/edit-note/:id', async (req,res)=>{
+    const {title, description} = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title, description})
+    res.redirect('/notes');
+})
+
+router.delete('/notes/delete/:id', async (req,res)=>{
+    await Note.findByIdAndDelete(req.params.id);
+    res.redirect('/notes');
+})
 
 module.exports = router;
